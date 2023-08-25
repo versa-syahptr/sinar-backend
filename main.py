946b65b3f442d9316134997e842432fa26debef8
Versa Syahputra <versa1220@gmail.com>
2023-08-25 14:27:06 +0700

    little fix

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-y", "--yolo", type=str, required=True, help="path to yolo model")
     parser.add_argument("-a", "--ab", type=str, required=True, help="path to analysis behavior model")
+    parser.add_argument("-d", "--device", type=str, default="cpu", help="device to run yolo model")
     parser.add_argument("-s", "--source", type=str, required=True, action="append", help="source(s) of video")
     parser.add_argument("-o", "--output", type=str, required=True, action="append", help="output stream(s)")
 
@@ -24,11 +25,11 @@ if __name__ == "__main__":
     ultralytics.checks()
 
     sinar = SINAR(args.yolo, args.ab)
-    
+
     try:
         for source, output in zip(args.source, args.output):
             logger.info(f"processing {source} -> {output}")
-            vi = VideoInfo.from_video_path(args.source)
+            vi = VideoInfo.from_video_path(source)
             streamer = RTMPStream(vi.width, vi.height, vi.fps).start(output)
 
             sinar.start_threaded(os.path.basename(source), source, streamto=streamer)
